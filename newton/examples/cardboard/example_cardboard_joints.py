f2c409aec2e6559d39028a5ec88a7d92dd70fdfc
Zeno Hamers <zeno.hamers@gmail.com>
2025-10-11 22:34:21 +0200

    tuned spring damper system - resembling cardboard

---
+++ newton/examples/cardboard/example_cardboard_joints.py
@@ -0,0 +1,176 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+###########################################################################
+# Example Basic Pendulum
+#
+# Shows how to set up a simulation of a simple double pendulum using the
+# newton.ModelBuilder() class.
+#
+# Command: python -m newton.examples basic_pendulum
+#
+###########################################################################
+
+import warp as wp
+
+import newton
+import newton.examples
+
+
+class CardboardJoint:
+    def __init__(self, viewer):
+        # setup simulation parameters first
+        self.fps = 100
+        self.frame_dt = 1.0 / self.fps
+        self.sim_time = 0.0
+        self.sim_substeps = 10
+        self.sim_dt = self.frame_dt / self.sim_substeps
+
+        self.viewer = viewer
+
+        builder = newton.ModelBuilder()
+
+        builder.add_articulation(key="cardboard_joint")
+
+        # define equilibrium position of the cardboard joint
+        self.joint_eq_pos = float(wp.radians(7.0))
+
+        # define carboard plane dimensions
+        hx = 0.10
+        hy = 0.40
+        hz = 0.003
+
+        # create first link
+        link_0 = builder.add_body(mass=0.2)
+        builder.add_shape_box(link_0, hx=hx, hy=hy, hz=hz)
+
+        # add joints
+        rot = wp.quat_from_axis_angle(wp.vec3(0.0, 0.0, 1.0), -wp.pi * 0.5)
+        builder.add_joint_revolute(
+            parent=-1,
+            child=link_0,
+            axis=wp.vec3(0.0, 1.0, 0.0),
+            parent_xform=wp.transform(p=wp.vec3(0.0, 0.0, 1.0), q=rot),  # rotate pendulum around the z-axis to appear sideways to the viewer
+            child_xform=wp.transform(p=wp.vec3(-hx, 0.0, 0.0), q=wp.quat_identity(dtype=wp.float32)), # make sure the joint attaches to the end of the rigid body
+            mode=newton.JointMode.TARGET_POSITION,
+            target=self.joint_eq_pos,
+            target_ke=50,
+            target_kd=0.7,
+        )
+
+        # add ground plane
+        builder.add_ground_plane()
+
+        # finalize model
+        self.model = builder.finalize()
+
+        self.solver = newton.solvers.SolverXPBD(self.model)
+
+        self.state_0 = self.model.state() # current state
+        self.state_1 = self.model.state() # t + dt state
+        self.control = self.model.control()
+        self.contacts = self.model.collide(self.state_0)
+
+        self.viewer.set_model(self.model)
+
+        # Set camera closer to the cardboard joint object
+        # Joint is at (0, 0, 1.0), so position camera at a good viewing angle
+        camera_pos = wp.vec3(1.5, 0.0, 1.1)  # Close position with slight offset
+        camera_pitch = -10.0  # Look slightly down
+        camera_yaw = -175.0   # Angle to view the joint from the side
+        self.viewer.set_camera(camera_pos, camera_pitch, camera_yaw)
+
+        # Enable joint visualization to show joint axes
+        self.viewer.show_joints = True
+
+        # not required for MuJoCo, but required for other solvers
+        newton.eval_fk(self.model, self.model.joint_q, self.model.joint_qd, self.state_0)
+
+        self.capture()
+
+    def capture(self):
+        if wp.get_device().is_cuda:
+            with wp.ScopedCapture() as capture:
+                self.simulate()
+            self.graph = capture.graph
+        else:
+            self.graph = None
+
+    def simulate(self):
+        for _ in range(self.sim_substeps):
+            # idea:
+            # 1. define an equilibrium positionfor the cardboard joints
+            # 2. use a spring damper system to control the joints (as if there was an actuator inside)
+
+            # Here we implement the controls of the joints!
+            self.state_0.clear_forces()
+
+            # apply forces to the model
+            self.viewer.apply_forces(self.state_0)
+
+            self.contacts = self.model.collide(self.state_0)
+            self.solver.step(self.state_0, self.state_1, self.control, self.contacts, self.sim_dt)
+
+            # swap states
+            self.state_0, self.state_1 = self.state_1, self.state_0
+
+    def step(self):
+        if self.graph:
+            wp.capture_launch(self.graph)
+        else:
+            self.simulate()
+
+        self.sim_time += self.frame_dt
+
+    def test(self):
+        # rough check that the cardboard joint links are in the correct area
+        newton.examples.test_body_state(
+            self.model,
+            self.state_0,
+            "cardboard joint links in correct area",
+            lambda q, qd: abs(q[0]) < 1e-5 and abs(q[1]) < 1.0 and q[2] < 5.0 and q[2] > 0.0,
+            [0, 1],
+        )
+
+        def check_velocities(_, qd):
+            # velocity outside the plane of the cardboard joint should be close to zero
+            check = abs(qd[0]) < 1e-4 and abs(qd[6]) < 1e-4
+            # velocity in the plane of the pendulum should be reasonable
+            check = check and abs(qd[1]) < 10.0 and abs(qd[2]) < 5.0 and abs(qd[3]) < 10.0 and abs(qd[4]) < 10.0
+            return check
+
+        newton.examples.test_body_state(
+            self.model,
+            self.state_0,
+            "cardboard joint links have reasonable velocities",
+            check_velocities,
+            [0, 1],
+        )
+
+    def render(self):
+        self.viewer.begin_frame(self.sim_time)
+        self.viewer.log_state(self.state_0)
+        self.viewer.log_contacts(self.contacts, self.state_0)
+
+        self.viewer.end_frame()
+
+if __name__ == "__main__":
+    # Parse arguments and initialize viewer
+    viewer, args = newton.examples.init()
+
+    # Create viewer and run
+    example = CardboardJoint(viewer)
+
+    newton.examples.run(example, args)
